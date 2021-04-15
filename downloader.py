13f40cc7e0b99aadaa5a9279df6b38ddeaaf24ed
Divyansh Asthana <76389464+asthana-jee@users.noreply.github.com>
2021-04-15 07:10:54 +0530

    Added Comments

--- downloader.py
+++ downloader.py
@@ -2,17 +2,21 @@
 from pytube import YouTube
 from pytube import Playlist
 
-playlist = Playlist("https://www.youtube.com/playlist?list=PLbu_fGT0MPsv9W38hFS_mTYbdOUOnidzG")
+playlist = Playlist("https://www.youtube.com/playlist?list=PLbu_fGT0MPsv9W38hFS_mTYbdOUOnidzG") #Edit this playlist with your playlist link
 
-links=[]
-for url in playlist:
+links=[] 		
+#Created a list to store links 
+for url in playlist:      
+	#This loop runs throughout the playlist to store every video's url
 	links.append(url)
 
-for link in links:  
+for link in links:  	  
+    #Download video one by one
     yt = YouTube(link)
     print(yt)
 
     stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    #Determines the quality of video
 
     try:
     	stream.download()
